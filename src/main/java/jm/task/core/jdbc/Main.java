539e2c96332526aa2184441539b34861bc4b68b7
alatair8@mail.ru <alatair8@mail.ru>
2020-09-19 14:53:07 +0300

    correct method "HibernateConnection" to "hibernateConnection"

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,10 +1,12 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
         UserDaoHibernateImpl userDaoHibernateImpl = new UserDaoHibernateImpl();
+        Util util = new Util();
         userDaoHibernateImpl.createUsersTable();
         userDaoHibernateImpl.saveUser("name1", "lastName1", (byte) 10);
         userDaoHibernateImpl.saveUser("name2", "lastName2", (byte) 20);
